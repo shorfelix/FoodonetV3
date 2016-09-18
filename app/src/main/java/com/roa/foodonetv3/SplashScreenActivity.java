300226e8a4cabaf1ba4d3789fad13f5a2058efe7
roa <roish2@gmail.com>
2016-09-18 13:00:30 +0300

    Initial commit

---
+++ app/src/main/java/com/roa/foodonetv3/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.roa.foodonetv3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        try {
+            Thread.sleep(4000);
+            Intent intent = new Intent(this, MainDrawerActivity.class);
+            startActivity(intent);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
