9e1ef9561147ed1c86e4c74e9e9f4b5f651d4b92
Alon Shulman <shulmanalon@gmail.com>
2017-03-28 23:34:52 +0300

    added sound to test notification

--- app/src/main/java/com/roa/foodonetv3/services/FoodonetGcmListenerService.java
+++ app/src/main/java/com/roa/foodonetv3/services/FoodonetGcmListenerService.java
@@ -2,6 +2,8 @@ package com.roa.foodonetv3.services;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -17,17 +19,19 @@ public class FoodonetGcmListenerService extends GcmListenerService {
     public static final String PUBLICATION_NUMBER = "pubnumber";
     @Override
     public void onMessageReceived(String s, Bundle bundle) {
-        Log.d(TAG, s);
+        Log.d(TAG, s+", :"+bundle.getString(PUSH_OBJECT_MSG));
         if(s.startsWith(getString(R.string.push_notification_prefix)) || s.compareTo(getString(R.string.notifications_server_id)) == 0) {
             String msg = bundle.getString(PUSH_OBJECT_MSG);
-            JSONObject jo = new JSONObject();
-            try {
-                jo = new JSONObject(msg);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            sendNotification(s,"New public");
-            /*//TODO chek from old app the parms
+//            JSONObject jo;
+//            new JSONObject();
+//            try {
+//                jo = new JSONObject(msg);
+//                Log.d(TAG,jo.toString());
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+            sendNotification(s,msg);
+            /*//TODO check from old app the params
             getContentResolver().insert(FooDoNetSQLProvider.URI_NOTIFICATIONS, notification.GetContentValuesRow());
             String basePath = getString(R.string.server_base_url);
             String subPath = getString(R.string.server_edit_publication_path).replace("{0}", String.valueOf(notification.get_publication_or_group_id()));
@@ -55,9 +59,11 @@ public class FoodonetGcmListenerService extends GcmListenerService {
 
     private void sendNotification(String title, String body) {
         Context context = getBaseContext();
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.drawer_notifications).setContentTitle(title)
-                .setContentText(body);
+                .setContentText(body)
+                .setSound(defaultSoundUri);
         NotificationManager mNotificationManager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(R.string.notifications_server_id, mBuilder.build());
